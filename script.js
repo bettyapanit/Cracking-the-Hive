6450700933dc054cc098256f66478c7232cd2a46
bettyapanit <bettyapanit@gmail.com>
2026-05-18 02:13:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 // נתוני המשחק
-// שימי לב: משושי התשובות (answers/) צריכים להיות 12 קבצי PNG נפרדים שחתכת בפוטושופ
-// כל קובץ צריך להיקרא בדיוק כמו התשובה (עם קו תחתון במקום רווחים)
 const gameData = [
-    { question: "כמה פרחים בממוצע צריכה דבורה לבקר כדי לייצר קילוגרם אחד של דבש?", answer: "4,000,000", letterImage: "letters/4000000.png", answerImage: "answers/4000000.png" },
-    { question: "כמה עיניים יש לדבורה בסך הכל?", answer: "5", letterImage: "letters/5.png", answerImage: "answers/5.png" },
-    { question: "מהו הגורם היחיד שקובע אם זחל נקבה יתפתח לפועלת או למלכה?", answer: "סוג המזון (מזון מלכות)", letterImage: "letters/סוג_המזון__מזון_מלכות_.png", answerImage: "answers/סוג_המזון__מזון_מלכות_.png" },
-    { question: "מהי המהירות הממוצעת (בק\"מ לשעה) שבה עפה דבורה עמוסה בצוף?", answer: "25", letterImage: "letters/25.png", answerImage: "answers/25.png" },
-    { question: "מהו שמו של ה\"דבק\" הטבעי שהדבורים מייצרות לאיטום סדקים בכוורת?", answer: "פרופוליס", letterImage: "letters/פרופוליס.png", answerImage: "answers/פרופוליס.png" },
-    { question: "כמה רגליים יש לדבורה?", answer: "6", letterImage: "letters/6.png", answerImage: "answers/6.png" },
-    { question: "כמה פעמים בממוצע מנפנפת דבורה בכנפיה בשנייה אחת בזמן מעוף?", answer: "200", letterImage: "letters/200.png", answerImage: "answers/200.png" },
-    { question: "מה תפקידו של \"ריקוד הדבורה\"?", answer: "לסמן לדבורים אחרות את כיוון הפרחים ביחס למיקום השמש", letterImage: "letters/לסמן_לדבורים_אחרות_את_כיוון_הפרחים_ביחס_למיקום_השמש.png", answerImage: "answers/לסמן_לדבורים_אחרות_את_כיוון_הפרחים_ביחס_למיקום_השמש.png" },
-    { question: "מהו החומר אותו מפרישות הדבורים מגופן כדי לבנות את תאי הכוורת?", answer: "דונג", letterImage: "letters/דונג.png", answerImage: "answers/דונג.png" },
-    { question: "כמה כפיות דבש מייצרת דבורה פועלת אחת במהלך כל תקופת חייה?", answer: "1/2", letterImage: "letters/0_5.png", answerImage: "answers/0_5.png" },
-    { question: "מהי הטמפרטורה (במעלות צלזיוס) שעליה שומרות הדבורים בתוך הכוורת?", answer: "35", letterImage: "letters/35.png", answerImage: "answers/35.png" },
-    { question: "לאיזה גובה מקסימלי (במטרים) מסוגלת דבורה להגיע?", answer: "9000", letterImage: "letters/9000.png", answerImage: "answers/9000.png" }
+    { question: "כמה פרחים בממוצע צריכה דבורה לבקר כדי לייצר קילוגרם אחד של דבש?", answer: "4,000,000", letterImage: "letters/L4000000.png", answerImage: "answers/A4000000.png" },
+    { question: "כמה עיניים יש לדבורה בסך הכל?", answer: "5", letterImage: "letters/L5.png", answerImage: "answers/A5.png" },
+    { question: "מהו הגורם היחיד שקובע אם זחל נקבה יתפתח לפועלת או למלכה?", answer: "סוג המזון (מזון מלכות)", letterImage: "letters/Lסוג_המזון__מזון_מלכות_.png", answerImage: "answers/Aסוג_המזון__מזון_מלכות_.png" },
+    { question: "מהי המהירות הממוצעת (בק\"מ לשעה) שבה עפה דבורה עמוסה בצוף?", answer: "25", letterImage: "letters/L25.png", answerImage: "answers/A25.png" },
+    { question: "מהו שמו של ה\"דבק\" הטבעי שהדבורים מייצרות לאיטום סדקים בכוורת?", answer: "פרופוליס", letterImage: "letters/Lפרופוליס.png", answerImage: "answers/Aפרופוליס.png" },
+    { question: "כמה רגליים יש לדבורה?", answer: "6", letterImage: "letters/L6.png", answerImage: "answers/A6.png" },
+    { question: "כמה פעמים בממוצע מנפנפת דבורה בכנפיה בשנייה אחת בזמן מעוף?", answer: "200", letterImage: "letters/L200.png", answerImage: "answers/A200.png" },
+    { question: "מה תפקידו של \"ריקוד הדבורה\"?", answer: "לסמן לדבורים אחרות את כיוון הפרחים ביחס למיקום השמש", letterImage: "letters/Lלסמן_לדבורים_אחרות_את_כיוון_הפרחים_ביחס_למיקום_השמש.png", answerImage: "answers/Aלסמן_לדבורים_אחרות_את_כיוון_הפרחים_ביחס_למיקום_השמש.png" },
+    { question: "מהו החומר אותו מפרישות הדבורים מגופן כדי לבנות את תאי הכוורת?", answer: "דונג", letterImage: "letters/Lדונג.png", answerImage: "answers/Aדונג.png" },
+    { question: "כמה כפיות דבש מייצרת דבורה פועלת אחת במהלך כל תקופת חייה?", answer: "1/2", letterImage: "letters/L0_5.png", answerImage: "answers/A0_5.png" },
+    { question: "מהי הטמפרטורה (במעלות צלזיוס) שעליה שומרות הדבורים בתוך הכוורת?", answer: "35", letterImage: "letters/L35.png", answerImage: "answers/A35.png" },
+    { question: "לאיזה גובה מקסימלי (במטרים) מסוגלת דבורה להגיע?", answer: "9000", letterImage: "letters/L9000.png", answerImage: "answers/A9000.png" }
 ];
 
 const correctSentence = "משהו מתוק מגיע";
